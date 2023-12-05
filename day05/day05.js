59cadf40a7a1a4c4b0802ca3bc3abea65f6e46af
Will Croteau <willcro@users.noreply.github.com>
2023-12-05 14:16:01 -0600

    Refactored day 5 so that parts 1 and 2 reuse code

--- day05/day05.js
+++ day05/day05.js
@@ -2,65 +2,38 @@
 
 const text = await Deno.readTextFile("./input.txt");
 
-function parseRange(range) {
-  const numbers = range.split(" ");
-  const destinationRangeStart = numbers[0] * 1;
-  const sourceRangeStart = numbers[1] * 1;
-  const length = numbers[2] * 1;
-
-  return {
-    sourceRangeStart: sourceRangeStart,
-    sourceRangeEnd: sourceRangeStart + length - 1,
-    offset: destinationRangeStart - sourceRangeStart
-  };
-}
-
-function parseGroup(group) {
-  const lines = group.split("\n");
-  const match = /([a-z]+)-to-([a-z]+) map:/.exec(lines[0]);
-  const sourceType = match[1];
-  const destinationType = match[2];
-  const mapper = (n) => {
-    for (let i = 1; i < lines.length; i++) {
-      let range = parseRange(lines[i]);
-      if (n >= range.sourceRangeStart && n <= range.sourceRangeEnd) {
-        return n + range.offset;
+function parseSeedsPart1(seedString) {
+  return /seeds: (.*)/.exec(seedString)[1]
+    .split(" ")
+    .map(it => it * 1)
+    .map(it => {
+      return {
+        start: it,
+        end: it
       }
-    }
-    return n;
-  }
-
-  return {
-    sourceType: sourceType,
-    destinationType: destinationType,
-    mapper: mapper
-  }
+    })
 }
 
-function parseInput(text) {
-  const groups = text.split("\n\n");
-  const seedStr = groups[0];
-  const seeds = /seeds: (.*)/.exec(seedStr)[1]
+function parseSeedsPart2(seedString) {
+  const seedNumbers = /seeds: (.*)/.exec(seedString)[1]
     .split(" ")
     .map(it => it * 1);
 
-  const mappers = groups.slice(1).map(parseGroup);
+  let seeds = [];
 
-  var locations = seeds.map((seed) => {
-    let out = seed;
-    mappers.forEach(mapper => out = mapper.mapper(out));
-    return out;
-  });
+  for (let i = 0; i < seedNumbers.length; i += 2) {
+    let rangeStart = seedNumbers[i];
+    let length = seedNumbers[i + 1];
+    seeds.push({
+      start: rangeStart,
+      end: rangeStart + length - 1
+    });
+  }
 
-  return locations;
-
+  return seeds;
 }
 
-console.log(parseInput(text).reduce((a, b) => Math.min(a, b), Infinity));
-
-// part 2
-
-function parseRange2(range) {
+function parseRange(range) {
   const numbers = range.split(" ");
   const destinationRangeStart = numbers[0] * 1;
   const sourceRangeStart = numbers[1] * 1;
@@ -73,7 +46,7 @@ function parseRange2(range) {
   };
 }
 
-function parseGroup2(group) {
+function parseGroup(group) {
   const lines = group.split("\n");
   const match = /([a-z]+)-to-([a-z]+) map:/.exec(lines[0]);
   const sourceType = match[1];
@@ -86,7 +59,7 @@ function parseGroup2(group) {
       next = [];
       for (let r = 0; r < inputRanges.length; r++) {
         const inputRange = inputRanges[r];
-        let range = parseRange2(lines[i]);
+        let range = parseRange(lines[i]);
         let overlap = getOverlap(inputRange, range);
         if (overlap.overlap != null) {
           out.push({
@@ -109,33 +82,16 @@ function parseGroup2(group) {
   }
 }
 
-function parseInput2(text) {
+function parseInput(text, seedParser) {
   const groups = text.split("\n\n");
   const seedStr = groups[0];
-  const seedNumbers = /seeds: (.*)/.exec(seedStr)[1]
-    .split(" ")
-    .map(it => it * 1);
-
-
-  let seeds = [];
-
-  for (let i = 0; i < seedNumbers.length; i += 2) {
-    let rangeStart = seedNumbers[i];
-    let length = seedNumbers[i + 1];
-    seeds.push({
-      start: rangeStart,
-      end: rangeStart + length - 1
-    });
-  }
-
-  const mappers = groups.slice(1).map(parseGroup2);
-
+  const seeds = seedParser(seedStr);
+  const mappers = groups.slice(1).map(parseGroup);
   let out = seeds;
   mappers.forEach(mapper => {
     out = mapper.mapper(out)
   });
   return out;
-
 }
 
 function getOverlap(range1, range2) {
@@ -154,22 +110,19 @@ function getOverlap(range1, range2) {
       start: range2.start,
       end: Math.min(range1.end, range2.end)
     };
-    
+
     let range1Remainder = [];
     let range2Remainder = [];
-    
     if (range1.start < range2.start) {
-      range1Remainder.push({start: range1.start, end: range2.start - 1});
+      range1Remainder.push({ start: range1.start, end: range2.start - 1 });
     }
-    
     if (range1.end > range2.end) {
-      range1Remainder.push({ start: range2.end + 1, end: range1.end});
+      range1Remainder.push({ start: range2.end + 1, end: range1.end });
     }
-    
     if (range2.end > range1.end) {
       range2Remainder.push({ start: range1.end + 1, end: range2.end });
     }
-    
+
     return {
       overlap: overlap,
       range1Remainder: range1Remainder,
@@ -187,8 +140,10 @@ function getOverlap(range1, range2) {
     ]
   };
 
-
-
 }
 
-console.log(parseInput2(text).map(it => it.start).reduce((a,b) => Math.min(a, b)))
+console.log(parseInput(text, parseSeedsPart1).map(it => it.start).reduce((a, b) => Math.min(a, b)));
+
+// part 2
+
+console.log(parseInput(text, parseSeedsPart2).map(it => it.start).reduce((a, b) => Math.min(a, b)))
