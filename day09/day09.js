8360695a5be63d10d869cafaa12907c67c873944
Will Croteau <willcro@users.noreply.github.com>
2023-12-09 14:40:21 -0600

    Cleaned up day 9 a bit

--- day09/day09.js
+++ day09/day09.js
@@ -1,30 +1,24 @@
 // part 1
 
 const text = await Deno.readTextFile("./input.txt");
-const lines = text.split("\n");
-
-function parseLine(line) {
-  return line.split(" ");
-}
+const rows = text.split("\n").map(line => line.split(" "));
 
 function findNext(row) {
-  return row.map((it, i) => choose(row.length, row.length - i) * it * Math.pow(-1, row.length - i + 1))
+  const n = row.length;
+  return row.map((it, i) => choose(n, n - i) * it * Math.pow(-1, n - i + 1))
     .reduce((a, b) => a + b, 0);
 }
 
 function factorial(n) {
-  if (n == 0) {
-    return 1;
-  }
-  return factorial(n - 1) * n;
+  return [...Array(n).keys()].map(i => i + 1).reduce((a,b) => a * b, 1);
 }
 
 function choose(n, k) {
   return factorial(n) / (factorial(n - k) * factorial(k));
 }
 
-console.log(lines.map(parseLine).map(findNext).reduce((a, b) => a + b, 0));
+console.log(rows.map(findNext).reduce((a, b) => a + b, 0));
 
 // part 2
 
-console.log(lines.map(parseLine).map(it => it.reverse()).map(findNext).reduce((a, b) => a + b, 0));
+console.log(rows.map(it => it.reverse()).map(findNext).reduce((a, b) => a + b, 0));
