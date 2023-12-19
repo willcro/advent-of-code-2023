59525be2288ce1dcdb7da44358b575813094bd7a
Will Croteau <willcro@users.noreply.github.com>
2023-12-19 11:47:00 -0600

    Added day 19 part 2

--- day19/day19.js
+++ day19/day19.js
@@ -14,19 +14,58 @@ function parseWorkflows(str) {
       rules: match[2].split(",").map(parseRule)
     }
   }).forEach(it => out[it.id] = it);
-  
+
   return out;
 }
 
 function parseRule(ruleStr) {
   let match = ruleReg.exec(ruleStr);
 
-  let condition = () => true;
+  let condition = (part) => {
+    return {
+      passed: part,
+      failed: null
+    }
+  };
+
   if (match[3] != undefined && match[3] == "<") {
-    condition = (part) => part[match[2]] < (match[4] * 1);
+    condition = (part) => {
+      // clone
+      let passed = JSON.parse(JSON.stringify(part));
+      let failed = JSON.parse(JSON.stringify(part));
+      let threshold = (match[4] * 1);
+
+      if (part[match[2]].min >= threshold) {
+        passed = null;
+      } else if (part[match[2]].max < threshold) {
+        failed = null;
+      } else {
+        passed[match[2]].max = threshold - 1;
+        failed[match[2]].min = threshold;
+      }
+
+      return { passed, failed };
+    };
   }
+
   if (match[3] != undefined && match[3] == ">") {
-    condition = (part) => part[match[2]] > (match[4] * 1);
+    condition = (part) => {
+      // clone
+      let passed = JSON.parse(JSON.stringify(part));
+      let failed = JSON.parse(JSON.stringify(part));
+      let threshold = (match[4] * 1);
+
+      if (part[match[2]].max <= threshold) {
+        passed = null;
+      } else if (part[match[2]].min > threshold) {
+        failed = null;
+      } else {
+        passed[match[2]].min = threshold + 1;
+        failed[match[2]].max = threshold;
+      }
+
+      return { passed, failed };
+    };
   }
 
   return {
@@ -41,56 +80,77 @@ function parseParts(str) {
     .map(line => {
       let match = partReg.exec(line);
       return {
-        x: match[1] * 1,
-        m: match[2] * 1,
-        a: match[3] * 1,
-        s: match[4] * 1,
+        x: { min: match[1] * 1, max: match[1] * 1 },
+        m: { min: match[2] * 1, max: match[2] * 1 },
+        a: { min: match[3] * 1, max: match[3] * 1 },
+        s: { min: match[4] * 1, max: match[4] * 1 },
       }
     })
 }
 
-function runPart1(workflows, allParts) {
-  let buckets = { in: allParts };
+function push(map, key, value) {
+  if (map[key] == undefined) {
+    map[key] = [];
+  }
+  map[key].push(value);
+}
+
+function run(workflows, parts) {
+  let buckets = { in: parts };
   let newBuckets = {};
-  
+
   let done = false;
   while (!done) {
     done = true;
-    
     Object.entries(buckets).forEach(bucket => {
-      // console.log(bucket)
       let id = bucket[0];
       let parts = bucket[1];
       let workflow = workflows[id];
       if (workflow == undefined) {
-        if (newBuckets[id] == undefined) {
-          newBuckets[id] = [];
-        }
-        parts.forEach(part => newBuckets[id].push(part));
+        parts.forEach(part => push(newBuckets, id, part));
         return;
       }
-      // console.log(parts)
       parts.forEach(part => {
-        let newBucket = workflow.rules.filter(wf => wf.condition(part))[0].result;
-        if (newBuckets[newBucket] == undefined) {
-          newBuckets[newBucket] = [];
-        }
-        if (newBucket != id) {
-          done = false;
-        }        
-        newBuckets[newBucket].push(part);
+        let remainder = part;
+        workflow.rules.forEach(rule => {
+          if (remainder == null) {
+            return;
+          }
+
+          let result = rule.condition(remainder);
+          if (result.passed != null) {
+            push(newBuckets, rule.result, result.passed);
+            done = false;
+          }
+
+          remainder = result.failed;
+        });
+
       });
     });
-    
+
     buckets = newBuckets;
     newBuckets = {};
   }
-  
+
   return buckets;
+}
+
+function sum(ranges) {
+  return ranges.map(part => part.x.min + part.m.min + part.a.min + part.s.min).reduce((a, b) => a + b, 0)
+}
+
+function combos(ranges) {
+  return ranges.map(r => (r.x.max - r.x.min + 1) * (r.m.max - r.m.min + 1) * (r.a.max - r.a.min + 1) * (r.s.max - r.s.min + 1))
+    .reduce((a, b) => a + b);
 }
 
 let workflows = parseWorkflows(sections[0]);
 let parts = parseParts(sections[1]);
-let out = runPart1(workflows, parts);
 
-console.log(out["A"].map(part => part.x + part.m + part.a + part.s).reduce((a,b) => a+b, 0))
+let part1 = run(workflows, parts);
+console.log(sum(part1.A));
+
+// part 2
+let part2 = run(workflows, [{ x: { min: 1, max: 4000 }, m: { min: 1, max: 4000 }, a: { min: 1, max: 4000 }, s: { min: 1, max: 4000 }, }]);
+console.log(combos(part2.A));
